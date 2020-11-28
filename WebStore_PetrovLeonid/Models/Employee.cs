5636e9dc1a80e3b735c710d5ca1d4931100e33b6
Leonid <Leoon111@gmail.com>
2020-11-28 10:06:08 +0300

    Первая модель

---
+++ WebStore_PetrovLeonid/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore_PetrovLeonid.Models
+{
+    /// <summary>
+    /// Сотрудники
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>Номер</summary>
+        public int Id { get; set; }
+        /// <summary>Имя</summary>
+        public string FirstName { get; set; }
+        /// <summary>Фамилия</summary>
+        public string LastName { get; set; }
+        /// <summary>Отчество</summary>
+        public string Patronymic { get; set; }
+        /// <summary>Возраст</summary>
+        public int Age { get; set; }
+
+    }
+}
