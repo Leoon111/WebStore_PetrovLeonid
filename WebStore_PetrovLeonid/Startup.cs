5ff2567077e7e2f762f3c631bd4b96a4ab2f3118
Leonid <Leoon111@gmail.com>
2020-11-28 05:12:49 +0300

    Использование конфигурации при обработке запросов

--- WebStore_PetrovLeonid/Startup.cs
+++ WebStore_PetrovLeonid/Startup.cs
@@ -7,18 +7,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace WebStore_PetrovLeonid
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,11 +29,13 @@ namespace WebStore_PetrovLeonid
 
             app.UseRouting();
 
+            var greetings = _configuration["greetings"];
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(greetings);
                 });
             });
         }
