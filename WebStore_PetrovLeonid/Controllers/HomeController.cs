f7388ba7f8ba012125ccff1cc30dfd032aae3338
Leonid <Leoon111@gmail.com>
2020-11-28 12:16:16 +0300

    Передача данных на представление в виде модели

--- WebStore_PetrovLeonid/Controllers/HomeController.cs
+++ WebStore_PetrovLeonid/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore_PetrovLeonid.Models;
 
 namespace WebStore_PetrovLeonid.Controllers
 {
     public class HomeController : Controller
     {
+        public static readonly List<Employee> _Employees = new()
+        {
+            new Employee() {Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 20},
+            new Employee() {Id = 1, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 40},
+            new Employee() {Id = 1, LastName = "Михайлов", FirstName = "Михаил", Patronymic = "Михайлович", Age = 60}
+        };
+
         public IActionResult Index()
         {
             //return Content("First controller action");
@@ -17,6 +25,11 @@ namespace WebStore_PetrovLeonid.Controllers
         public IActionResult SecondAction()
         {
             return Content("Second controller action");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
         }
     }
 }
